0018ea23a2ffb4d91773f9bd12c07e90579b77e5
Lucas Bourcier <lbourcier@sio.local>
2020-11-03 11:40:28 +0100

    app folder

---
+++ src/Index.php
@@ -0,0 +1,14 @@
+<?pgp
+
+<!doctype html>
+<html>
+<head>
+	<meta charset="urf-8">
+	<title>site perso</title>
+</head>
+<body>
+<?php
+$displaymenu($menu);
+?>
+</body>
+</html>
---
+++ src/app/fonction.php
@@ -0,0 +1,11 @@
+<?php
+function displaymenu(array $items) {
+	$res='<ul>';
+	$foreach($items as $item){
+		$res.="<li>$item</li>";
+		}
+	$res.='</ul>'
+	echo $res;
+}
+
+?>
