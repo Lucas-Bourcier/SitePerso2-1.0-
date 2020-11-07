141bef8d9ba9f15d42cf61c14a93bbf1fffcba19
Lucas <lucas.bourcier@sts-sio-caen.info>
2020-11-07 15:51:08 +0100

    cont.php

---
+++ propos.php
@@ -0,0 +1,27 @@
+<?
+echo include'commannde.php'
+?>
+<a href="<?php $url_propos; ?>" ><?php $Propos; ?></a>
+
+<!DOCTYPE html>
+<html>
+    <head>
+	        <meta charset="utf-8">
+        <title>BOURCIER Lucas page profesionnelle</title>
+		 <link   rel="stylesheet" type="text/css" href="PagePerso.css">
+	</head>
+		  <body id="fond">
+<h1 id="NP">BOURCIER Lucas</h1>
+<h2 id="NP">Développeur</h2>
+<h3 id="NP">De la création de contenu depuis 2020</h3>
+	<header id="head">
+		<ol>
+	<li><a id="NPG" href="propos.php" target="_blank">A propos	</li>
+	<li><a id="NPG" href="Exp.php" target="_blank">Expériences	</li>
+	<li><a id="NPG" href="comp.php" target="_blank">Compétences	</li>
+	<li><a id="NPG" href="form.php" target="_blank">Formation	</li>
+	<li><a id="NPG" href="cont.php" target="_blank">Contact		</li>
+		</ol>
+	</header>
+		  </body>
+</html>
