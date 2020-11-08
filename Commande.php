358d96c58aaacda454710e8afe078acc3b87289c
Lucas <lucas.bourcier@sts-sio-caen.info>
2020-11-08 14:38:57 +0100

    Form.php

---
+++ Commande.php
@@ -0,0 +1,17 @@
+<?
+$propos="Voir page suivante";
+$url_propos="http://localhost/LBOUR/propos.php"
+
+$Comp="Voir page suivante";
+$url_Comp="http://localhost/LBOUR/Comp.php";
+
+$Form="Voir page suivante";
+$url_Form="http://localhost/LBOUR/Form.php";
+
+$Cont="Voir page suivante";
+$url_Cont="http://localhost/LBOUR/Cont.php";
+
+$Exp="Voir page suivante";
+$url_Exp="http://localhost/LBOUR/Exp.php";
+
+?>
