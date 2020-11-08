f0dcd508461e96d70481ac9ad3f24c5e27eb32aa
Lucas <lucas.bourcier@sts-sio-caen.info>
2020-11-08 14:35:02 +0100

    comp.php

--- Form.php
+++ Form.php
@@ -1,3 +1,35 @@
 <?
-echo include'commande.php'
+echo include'commannde.php'
 ?>
+<a href="<?php $url_propos; ?>" ><?php $Propos; ?></a>
+
+<!DOCTYPE html>
+<html>
+    <head>
+	        <meta charset="utf-8">
+        <title>BOURCIER Lucas page profesionnelle</title>
+		 <link   rel="stylesheet" type="text/css" href="bbb.css">
+	</head>
+		  <body id="fond">
+		  
+		  
+			<nav id="aa">
+			<ul>
+				<li><a href="propos.php">A propos</a></li>
+				<li><a href="Comp.php">Compétences</a></li>
+				<li><a href="Form.php">Formation</a></li>
+				<li><a href="Exp.php">Expériences</a></li>
+				<li><a href="Index.php">Contact</a></li>
+			</ul>
+			</nav>
+			<h1 id="a">Formations</h1>
+			<ul id="bb">
+				<li>2020-2022</li>
+				<li>2020</li>
+				<li>2019</li>
+			</ul>
+			<p id="p1">Formation de Developpeur en BTS SIO</p>
+			<p id="p2">Baccalauréat professionnel Technicien d'Usinage </p>
+			<p id="p3">Brevet d'études professionnelles de Technicien d'Usinage</p>
+		  </body>
+</html>
--- propos.php
+++ propos.php
@@ -8,20 +8,29 @@ echo include'commannde.php'
     <head>
 	        <meta charset="utf-8">
         <title>BOURCIER Lucas page profesionnelle</title>
-		 <link   rel="stylesheet" type="text/css" href="PagePerso.css">
+		 <link   rel="stylesheet" type="text/css" href="bbb.css">
 	</head>
-		  <body id="fond">
-<h1 id="NP">BOURCIER Lucas</h1>
-<h2 id="NP">Développeur</h2>
-<h3 id="NP">De la création de contenu depuis 2020</h3>
-	<header id="head">
-		<ol>
-	<li><a id="NPG" href="propos.php" target="_blank">A propos	</li>
-	<li><a id="NPG" href="Exp.php" target="_blank">Expériences	</li>
-	<li><a id="NPG" href="comp.php" target="_blank">Compétences	</li>
-	<li><a id="NPG" href="form.php" target="_blank">Formation	</li>
-	<li><a id="NPG" href="cont.php" target="_blank">Contact		</li>
-		</ol>
-	</header>
+		  <body id="fond2">
+		  
+		  
+			<nav id="aa">
+			<ul>
+				<li><a href="propos.php">A propos</a></li>
+				<li><a href="Comp.php">Compétences</a></li>
+				<li><a href="Form.php">Formation</a></li>
+				<li><a href="Exp.php">Expériences</a></li>
+				<li><a href="Index.php">Contact</a></li>
+			</ul>
+			</nav>
+			<div id="ex1"></div>
+			
+			<h1 id="a">A Propos</h1>
+<h1 id="titre""NP"><br>Bienvenue sur mon site</h1>
+
+<p id="Texte"><br><br>Je suis BOURCIER Lucas, Etudiant en développement Informatique<br><br>
+Je développe petit à petit mes compétences et donc mon site dans le même temps.</p>
+
+
+<p id="Citation"><br><br><br>[De la création de contenu depuis 2020]</p>
 		  </body>
 </html>
