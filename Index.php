647c58050d8f4c649126a5486e2bbe685b51aba0
Lucas-Bourcier <73880241+Lucas-Bourcier@users.noreply.github.com>
2020-11-18 23:31:11 +0100

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,18 +1,36 @@
-<?php
-	require('vendor/autoload.php');
+<?
+echo include'commannde.php'
 ?>
+<a href="<?php $url_propos; ?>" ><?php $Propos; ?></a>
 
-<?php
-
-<!doctype html>
+<!DOCTYPE html>
 <html>
-<head>
-	<meta charset="urf-8">
-	<title>site perso</title>
-</head>
-<body>
-<?php
-$displaymenu($menu);
-?>
-</body>
+    <head>
+	        <meta charset="utf-8">
+        <title>BOURCIER Lucas page profesionnelle</title>
+		 <link   rel="stylesheet" type="text/css" href="bbb.css">
+	</head>
+		  <body id="fond2">
+		  
+		  
+			<nav id="aa">
+			<ul>
+				<li><a href="propos.php">A propos</a></li>
+				<li><a href="Comp.php">Compétences</a></li>
+				<li><a href="Form.php">Formation</a></li>
+				<li><a href="Exp.php">Expériences</a></li>
+				<li><a href="Cont.php">Contact</a></li>
+			</ul>
+			</nav>
+			<div id="ex1"></div>
+			
+			<h1 id="a">A Propos</h1>
+<h1 id="titre""NP"><br>Bienvenue sur mon site</h1>
+
+<p id="Texte"><br><br>Je suis BOURCIER Lucas, Etudiant en développement Informatique<br><br>
+Je développe petit à petit mes compétences et donc mon site dans le même temps.</p>
+
+
+<p id="Citation"><br><br><br>[De la création de contenu depuis 2020]</p>
+		  </body>
 </html>
