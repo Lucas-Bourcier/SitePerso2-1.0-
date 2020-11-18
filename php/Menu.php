280c17fb0136a29eb9ba03e31742ba379e5de5c5
Lucas <lucas.bourcier@sts-sio-caen.info>
2020-11-18 23:52:53 +0100

    php

---
+++ php/Menu.php
@@ -0,0 +1,20 @@
+<!Doctype html>
+<html>
+	<head>
+		<title>aaa</title>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" href="AAA.css"/>
+	</head>
+	<body>
+			<nav id="aa">
+			<ul>
+				<li><a href="(lien du doc php)">A propos</a></li>
+				<li><a href="(lien du doc php)">Compétences</a></li>
+				<li><a href="(lien du doc php)">Formation</a></li>
+				<li><a href="(lien du doc php)">Expériences</a></li>
+				<li><a href="(lien du doc php)">Contact</a></li>
+			</ul>
+			</nav>
+		<p> Texte </p>
+	</body>
+</html>
